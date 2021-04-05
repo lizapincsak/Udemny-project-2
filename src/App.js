d374798a0e1d5cae64d591626df47dddb589ddc3
lizapincsak <liza.pincsak@gmail.com>
2021-04-04 21:46:53 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import UserInput from './components/UserInput';
-import UserOutput from './components/UserOutput';
+import Validation from './validation/Validation';
+import CharComponent from './Char/CharComponent';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    flowers: [
-      {flowerName: 'Lily'},
-      {flowerName: 'Rose'},
-      {flowerName: 'Lilac'}
-    ]
+    content: ''
   }
+
+  handleOnChange = (e) => {
+   this.setState({[e.target.name]: e.target.value})
+  }
+
+  deleteCharacterHandler = (index) => {
+    const text = this.state.content.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({content: updatedText})
+  }
+
+
   render(){
+
+    const charList = this.state.content.split('').map((char, index) => {
+      return <CharComponent 
+      character={char} 
+      key={index}
+      clicked={()=> this.deleteCharacterHandler(index)}/>
+    })
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Favorite Flowers</h1>
-        <UserInput />
-        <UserOutput />
-      </header>
+      <div style ={{textAlign: "center", height: '100vh'}}>
+        <form onSubmit = {this.handleOnSubmit} style={{marginTop: '50vh'}}>
+          <input 
+          type = 'text' 
+          name = 'content' 
+          value = {this.state.content} 
+          onChange = {this.handleOnChange}
+          />
+          <p>{this.state.content}</p>
+          <Validation inputLength={this.state.content.length}/>
+          {charList}
+      </form>
     </div>
   );
 }
